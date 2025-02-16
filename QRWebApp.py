783340b6afd834eb95cae08b57213f2e54187fad
Aditya Ranjan <adityaranjan_2004@outlook.com>
2025-02-16 19:58:18 +0530

    Added requirements.txt

--- QRWebApp.py
+++ QRWebApp.py
@@ -6,4 +6,4 @@ st.markdown("Free QR code maker does not expire")
 URL = st.text_input("Add your URL", placeholder = "Add it here(ofcourse)")
 if st.button("Reset", type="primary"):
     img = getImage(URL)
-st.image("newQR.png")
+    QRimage = st.image("newQR.png")
