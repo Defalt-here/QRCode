9f2642e56a97a01f8303fc14af63891d5e895324
Aditya Ranjan <adityaranjan_2004@outlook.com>
2025-02-16 20:22:35 +0530

    Added download functionality

--- QRWebApp.py
+++ QRWebApp.py
@@ -1,4 +1,5 @@
 #from QRCodeGen import getImage
+import io
 import pyqrcode
 from pyqrcode import QRCode
 import streamlit as st
@@ -7,10 +8,20 @@ def makeQR(URL):
     return qr
 def getImage(URLString):
     img = makeQR(URLString).png('newQR.png',scale = 8)
-    return img
+    qr = makeQR(URLString)
+    img_bytes = io.BytesIO()
+    qr.png(img_bytes, scale=8)
+    img_bytes.seek(0)
+    return img_bytes.getvalue()
 st.title("Super cool QR code generator")
 st.markdown("Free QR code maker does not expire")
 URL = st.text_input("Add your URL", placeholder = "Add it here(ofcourse)")
-if st.button("Reset", type="primary"):
+if st.button("Make QR", type="primary"):
     img = getImage(URL)
     QRimage = st.image("newQR.png")
+    st.download_button(
+        label="Download QR Code",
+        data=img,
+        file_name="QRCode.png",
+        mime="image/png"
+    )
