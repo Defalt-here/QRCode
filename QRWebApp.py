3873a144890ea732f1071e544668aa14da6e4ba0
Aditya Ranjan <adityaranjan_2004@outlook.com>
2025-02-16 19:46:25 +0530

    initial commit

--- QRWebApp.py
+++ QRWebApp.py
@@ -1,9 +1,9 @@
-import QRCodeGen as QR
+from QRCodeGen import getImage
 import streamlit as st
 
 st.title("Super cool QR code generator")
 st.markdown("Free QR code maker does not expire")
 URL = st.text_input("Add your URL", placeholder = "Add it here(ofcourse)")
 if st.button("Reset", type="primary"):
-    img = QR.getImage(URL)
+    img = getImage(URL)
 st.image("newQR.png")
