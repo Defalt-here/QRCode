6e0f5884a0dbdbcc1eb46fae5a8e84b638263724
Aditya Ranjan <adityaranjan_2004@outlook.com>
2025-02-16 20:03:53 +0530

    sum

--- QRWebApp.py
+++ QRWebApp.py
@@ -1,6 +1,13 @@
-from QRCodeGen import getImage
+#from QRCodeGen import getImage
+import pyqrcode
+from pyqrcode import QRCode
 import streamlit as st
-
+def makeQR(URL):
+    qr = QRCode.create(URL)
+    return qr
+def getImage(URLString):
+    img = makeQR(URLString).png('newQR.png',scale = 8)
+    return img
 st.title("Super cool QR code generator")
 st.markdown("Free QR code maker does not expire")
 URL = st.text_input("Add your URL", placeholder = "Add it here(ofcourse)")
