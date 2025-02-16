5b002fcdd770f2f9347256697d71a10d32b3a140
Aditya Ranjan <adityaranjan_2004@outlook.com>
2025-02-16 19:49:41 +0530

    second commit

--- QRCodeGen.py
+++ QRCodeGen.py
@@ -1,8 +1,6 @@
-import pyqrcode 
 from pyqrcode import QRCode
-import png
 def makeQR(URL):
-    qr = pyqrcode.create(URL)
+    qr = QRCode.create(URL)
     return qr
 def getImage(URLString):
     img = makeQR(URLString).png('newQR.png',scale = 8)
